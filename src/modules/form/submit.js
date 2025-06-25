ac16544cbd9b74ade9350ec78ff8748e3780afe0
Breno Soriani <breno.soriani@gmail.com>
2025-06-25 17:06:30 -0300

    Enviando para API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,9 +1,13 @@
 const form = document.querySelector("form");
 const dataInput = document.querySelector("#date-agendamento");
 const horarioInput = document.querySelector("#time-agendamento");
+const petName = document.getElementById("#pet-name");
+const name = document.getElementById("#owner-name");
+const service = document.getElementById("#service");
+
 import dayjs from "dayjs";
 
-const diaAtual = dayjs(new Date()).format("DD-MM-YYYY");
+const diaAtual = dayjs(new Date()).format("MM-DD-YYYY");
 const horaAtual = dayjs(new Date()).format("HH:mm");
 
 //definir a data e horário mínima
@@ -13,16 +17,35 @@ horarioInput.min = horaAtual;
 form.onsubmit = (e) => {
   e.preventDefault();
 
-  const dataSelecionada = dataInput.value;
-  const horarioSelecionado = horarioInput.value;
+  // Limpa o container antes de criar novamente
+  scheduleContainer.innerHTML = "";
 
-  if (dataSelecionada < diaAtual) {
-    alert("A data selecionada não pode ser anterior ao dia de hoje.");
-    return;
-  }
+  try {
+    const name = name.value.trim();
+    const petName = petName.value.trim();
+    const service = service.value;
 
-  if (horarioSelecionado < horaAtual) {
-    alert("O horário selecionado não pode ser anterior ao horário atual.");
+    if (!name || !petName || !service) {
+      alert("Por favor, preencha todos os campos obrigatórios.");
+      return;
+    }
+    //recupera os horários selecionados
+    const dataSelecionada = dataInput.value;
+    const horarioSelecionado = horarioInput.value;
+
+    if (dataSelecionada < diaAtual) {
+      alert("A data selecionada não pode ser anterior ao dia de hoje.");
+      return;
+    }
+
+    if (horarioSelecionado < horaAtual) {
+      alert("O horário selecionado não pode ser anterior ao horário atual.");
+    }
+  } catch (error) {
+    console.error("Erro ao submeter o formulário:", error);
+    alert(
+      "Ocorreu um erro ao submeter o formulário. Por favor, tente novamente."
+    );
   }
 };
 
